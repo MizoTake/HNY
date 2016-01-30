2445adb795acf2583592f5af68dc6675aae2bb31
unknown <>
2016-01-31 02:11:31 +0900

    作業中3

---
+++ HNY/Assets/Prog/mbparticletest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mbparticletest : MonoBehaviour {
+
+	public Transform m_BaseTrans;
+	public Transform m_TargetTrans;
+	public mbParticleController m_Particle;
+
+	void OnGUI () {
+
+		for ( int i = 0;i < 6;++i ) {
+			if ( GUILayout.Button ( "Player" + ( i + 1 ).ToString () ) ) {
+				m_Particle.RunParticleEfffect ( i, m_BaseTrans.position, m_TargetTrans.position );
+			}
+		}
+
+	}
+}
