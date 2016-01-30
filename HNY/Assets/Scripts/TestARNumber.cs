f474d742e4f9ff5cd345e566fbb47cfcf3deb7fc
TakeruMizoguchi <mizotake123@gmail.com>
2016-01-30 18:57:09 +0900

    画像を認識させて、数値を取得する処理の実装

---
+++ HNY/Assets/Scripts/TestARNumber.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestARNumber : SingletonMonoBehaviour<TestARNumber> {
+    
+    private int _imageNumber;
+    private Text _textObj;
+    
+    public int ImageNumber{ get{ return _imageNumber; } set{ _imageNumber = value; } }
+
+	// Use this for initialization
+	void Start () {
+        _textObj = GetComponent<Text>();
+	   _imageNumber = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if(_imageNumber != -1){
+           _textObj.text = "" + _imageNumber;
+       }
+	}
+}
---
+++ HNY/Assets/Scripts/TextNumber.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextNumber : MonoBehaviour {
+    
+    public int _stepYear;
+
+	// Use this for initialization
+	void Start () {
+	   TestARNumber.Instance.ImageNumber = _stepYear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
